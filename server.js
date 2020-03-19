199478f9a9035e9df59cbdc50e2962e236fe8c34
aptaabye <santiorellana@live.ca>
2020-03-18 22:56:38 -0400

    ninth commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "client/build")));
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://protected-forest-85499.herokuapp.com",
   credentials: true
 }));
 app.use(cookieParser());
