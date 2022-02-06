6f3b9ae26b6c4d8a22bec5d53e7b9d2eb70997e9
aptaabye <santiorellana@live.ca>
2022-02-06 17:16:52 -0500

    revamp

--- herokuredirect.js
+++ herokuredirect.js
@@ -1,5 +1,5 @@
 const herokuredirect = function(req, res, next) {
-  if (process.env.NODE_ENV != 'development' && req.url === 'https://santiagoorellana.herokuapp.com') {
+  if (process.env.NODE_ENV != 'development' && req.url === 'https://santiagoorellana.herokuapp.com/home') {
        return res.redirect("https://www.santiagoorellana.com/home");
     }
     next();
