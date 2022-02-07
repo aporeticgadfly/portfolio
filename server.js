8e059130558aadce6ae9f16bf3f848942abb6d73
aptaabye <santiorellana@live.ca>
2022-02-07 08:30:25 -0500

    added status code for home route

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ let transport = nodemailer.createTransport({
 });
 
 app.get("/home", function(req, res) {
-  res.status(200);
+  res.status(200).end();
 });
 
 app.get('/linkedin', function(req, res) {
