bc6751cf39b82e01960b5bdd280a95f23a36a56e
aptaabye <santiorellana@live.ca>
2022-02-07 14:38:37 -0500

    modified cors func

--- forceHttps.js
+++ forceHttps.js
@@ -1,5 +1,4 @@
 const forceHttps = function(req, res, next) {
-  console.log(req.secure);
   if (process.env.NODE_ENV != 'development' && !req.secure) {
        return res.redirect("https://" + req.headers.host + req.url);
     }
