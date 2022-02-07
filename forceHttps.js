2089eaf200f8135767516bb714df778c7291c905
aptaabye <santiorellana@live.ca>
2022-02-07 05:27:42 -0500

    revamp

--- forceHttps.js
+++ forceHttps.js
@@ -1,5 +1,5 @@
 const forceHttps = function(req, res, next) {
-  console.log(req);
+  console.log(req.secure);
   if (process.env.NODE_ENV != 'development' && !req.secure) {
        return res.redirect("https://" + req.headers.host + req.url);
     }
--- herokuredirect.js
+++ herokuredirect.js
@@ -1,6 +1,5 @@
 const herokuredirect = function(req, res, next) {
-  console.log(req.url);
-  if (process.env.NODE_ENV != 'development' && req.url === 'https://santiagoorellana.herokuapp.com/home') {
+  if (process.env.NODE_ENV != 'development' && req.headers.host === 'santiagoorellana.herokuapp.com') {
        return res.redirect("https://www.santiagoorellana.com/home");
     }
     next();
